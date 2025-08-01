65e35695743840834e4f518685f6f5a948766d9c
DevJoshCodes <devjoshcodes@gmail.com>
2025-08-01 20:06:16 +0800

    Update config file generation to random

--- neural_network_class.py
+++ neural_network_class.py
@@ -1,8 +1,10 @@
 import numpy as np
 import yaml
+import random
 
 class FeedforwardNeuralNetwork:
     def __init__(self, configuration_file):
+        self.supported_activation_functions = ['relu', 'softmax']
         self.config = self._get_config(configuration_file)
         self.layers = self.config.get('layers')
         self._validate_structure()
@@ -14,19 +16,19 @@ class FeedforwardNeuralNetwork:
         sample_config = {
             "layers": [
                 {
-                    "neurons": 3
-                },
-                {
-                    "neurons": 4,
-                    "activation": "relu"
-                },
-                {
-                    "neurons": 2,
-                    "activation": "softmax"
+                    "neurons": random.randint(1, 10)
                 }
             ]
         }
-        
+
+        for i in range(random.randint(1, 10)):
+            sample_config["layers"].append(
+                {
+                    "neurons": random.randint(1, 10),
+                    "activation": random.choice(self.supported_activation_functions)
+                }
+            )
+
         try:
             with open(configuration_file, 'r') as config_file:
                 config = yaml.safe_load(config_file)
@@ -42,7 +44,7 @@ class FeedforwardNeuralNetwork:
     ## Validation
     def _handle_exception(self, exception):
         print(f"{type(exception).__name__}: {exception}")
-        print("If you think that your config file is corrupted or don't know how to fix it, just delete the config file and I'll regenerate a sample one!")
+        print("If you think that your config file is corrupted or don't know how to fix it, just delete the config file and a new sample one will be generated!")
         exit()
 
     def _validate_structure(self):
@@ -53,7 +55,6 @@ class FeedforwardNeuralNetwork:
         if not isinstance(input_neurons, int) or input_neurons <= 0:
             self._handle_exception(ValueError("layer 0 must have a 'neurons' key with a positive integer value"))
 
-        supported_activation_functions = ['relu', 'softmax']
         layers = self.layers[1:] # Exclude the first layer from validation specifically activation validation
         for index, layer in enumerate(layers, start=1):
             neurons = layer.get('neurons')
@@ -62,8 +63,8 @@ class FeedforwardNeuralNetwork:
                 self._handle_exception(ValueError(f"layer {index} must have a 'neurons' key with a positive integer value"))
             elif activation_function is None or activation_function == '':
                 self._handle_exception(ValueError(f"layer {index} must have an 'activation' key with a non-empty string value"))
-            elif activation_function not in supported_activation_functions:
-                self._handle_exception(ValueError(f"layer {index} has an unsupported activation function: {activation_function}. Supported activation functions are: {supported_activation_functions}"))
+            elif activation_function not in self.supported_activation_functions:
+                self._handle_exception(ValueError(f"layer {index} has an unsupported activation function: {activation_function}. Supported activation functions are: {self.supported_activation_functions}"))
 
     def _validate_input(self, activation):
         neurons = self.layers[0].get('neurons')
